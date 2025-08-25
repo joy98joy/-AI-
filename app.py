df81f3dbd7154f9daf5e5752784e2646d7011d3c
joy98joy <ss444555@naver.com>
2025-08-25 16:11:38 +0900

    Commit Message

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+import math
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+
+def hello_world():
+
+    return "Hello, DevOps!"
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8080)
